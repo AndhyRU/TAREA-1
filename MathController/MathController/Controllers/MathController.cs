713e0cf9a0eae64c8fdbd2cf60419a3820d87d58
AndhyRU <andhy.urena@gmail.com>
2026-08-23 16:53:15 -0400

    Inicializa proyecto Web API MathControlle y agrega MathControlle.cs

---
+++ MathController/MathController/Controllers/MathController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+    }
+}
