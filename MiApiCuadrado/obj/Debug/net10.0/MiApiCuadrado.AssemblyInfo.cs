99e74717ac1faf16058c38f023c4edb5ef711139
AndhyRU <andhy.urena@gmail.com>
2026-08-30 17:52:39 -0400

    refactor(api): actualizar modelo y probar apis Actualiza propiedades de estudiantes a no nulas y ejecuta pruebas finales de ambas APIs.

--- MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
+++ MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e37f43a3e6481f61712e1760e2c2a319ba9b553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32717d50b9251456126185dba5b28867ded961bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
