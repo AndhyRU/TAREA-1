a486aa3705eb7d1d5947b4cb957d7e5e3e9eb07c
AndhyRU <andhy.urena@gmail.com>
2026-09-06 10:03:42 -0400

    build(deploy): publicar aplicación en somee

--- MiApiCuadrado/Models/Estudiante.cs
+++ MiApiCuadrado/Models/Estudiante.cs
@@ -3,13 +3,13 @@ namespace MiApiCuadrado.Models
     public class Estudiante
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Matricula { get; set; }
-        public string Carrera { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Matricula { get; set; } = string.Empty;
+        public string Carrera { get; set; } = string.Empty;
         public int CantidadMaterias { get; set; }
         public int Edad { get; set; }
-
 
     }
 }
--- MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
+++ MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32717d50b9251456126185dba5b28867ded961bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e74717ac1faf16058c38f023c4edb5ef711139")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
