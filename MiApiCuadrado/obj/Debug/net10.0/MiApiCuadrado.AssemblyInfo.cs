ec18c9916faf3ab94f536b6c103b1b1b79e3da77
AndhyRU <andhy.urena@gmail.com>
2026-09-06 20:26:16 -0400

    build(deploy): publicar api en azure app service Elimina publicación previa y crea una nueva para desplegar en Azure App Service.

--- MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
+++ MiApiCuadrado/obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7052116ae5636796f343d2f8627addfc4ecc76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc82c03aac85530010f4417f1dfd7b989de0f59")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
